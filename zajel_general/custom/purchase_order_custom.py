5e4632b6434a8c06500cfb90e348ccc2e10b497f
erp <abc@gmail.com>
2025-03-03 16:36:21 +0500

    Coment PO Custom

--- zajel_general/custom/purchase_order_custom.py
+++ zajel_general/custom/purchase_order_custom.py
@@ -1,7 +1,7 @@
 import frappe
 
 def validate(self, method):
-            
-    for item in self.items:
-        if not item.material_request:
-            frappe.throw("Material Request is mandatory for creating Purchase Order")
+	pass
+    #for item in self.items:
+        #if not item.material_request:
+            #frappe.throw("Material Request is mandatory for creating Purchase Order")
