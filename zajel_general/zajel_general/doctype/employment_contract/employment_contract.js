c5969b7734962e6d114e83b8a9e16f7425f2488e
Hussain <rana.h10111992@gmail.com>
2026-02-09 17:53:38 +0500

    Deleted Employment Contract from local

--- zajel_general/zajel_general/doctype/employment_contract/employment_contract.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2026, Hussain and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Employment Contract", {
-// 	refresh(frm) {
-
-// 	},
-// });
--- zajel_general/zajel_general/doctype/employment_contract/employment_contract.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2026, Hussain and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class EmploymentContract(Document):
-	pass
