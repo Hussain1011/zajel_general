5a4fa7cbc195c51c88f5e23a5681f8f2b9482fc3
Hussain <rana.h10111992@gmail.com>
2026-04-07 16:13:08 +0500

    Owner Dashboards 1

--- zajel_general/hooks.py
+++ zajel_general/hooks.py
@@ -26,7 +26,7 @@ app_license = "mit"
 
 # include js, css files in header of desk.html
 # app_include_css = "/assets/zajel_general/css/zajel_general.css"
-# app_include_js = "/assets/zajel_general/js/zajel_general.js"
+# app_include_js = "/assets/zajel_general/js/entity-dashboard.js"
 
 # include js, css files in header of web template
 # web_include_css = "/assets/zajel_general/css/zajel_general.css"
